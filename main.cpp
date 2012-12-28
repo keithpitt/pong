f3d869dfd7ca7af16ec86e42c4ed9abeadd336fd
Keith Pitt <me@keithpitt.com>
2012-12-28 12:01:21 +1100

    Yay, a white box!

--- main.cpp
+++ main.cpp
@@ -10,9 +10,27 @@
 #include <GL/glu.h>
 #endif
 
-int main(int argc, char** argv) {
-  std::cout << "Hello World!";
+void init() {
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0.0, 1280, 720, 1.0, -1.0, 1.0);
+}
 
+void draw() {
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  glBegin(GL_QUADS);
+    glVertex2f(0,0);
+    glVertex2f(250,0);
+    glVertex2f(250,250);
+    glVertex2f(0,250);
+  glEnd();
+
+  glFlush();
+};
+
+int main(int argc, char** argv) {
   SDL_Init(SDL_INIT_EVERYTHING);
   SDL_Surface * screen = SDL_SetVideoMode(1280, 720, 32, SDL_SWSURFACE|SDL_OPENGL);
 
@@ -20,8 +38,11 @@ int main(int argc, char** argv) {
   uint32_t start;
   SDL_Event event;
 
+  init();
+
   while(running) {
     start = SDL_GetTicks();
+    draw();
 
     while(SDL_PollEvent(&event)) {
       if(event.type == SDL_QUIT) {
@@ -29,6 +50,8 @@ int main(int argc, char** argv) {
         break;
       }
     }
+
+    SDL_GL_SwapBuffers();
   }
 
   return 0;
